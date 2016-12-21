d90f6076269cb798f835e7121bd001f7c1cee9ef
hpan <hao_pan@outlook.com>
2016-12-21 15:33:58 +0800

    restructure repository

--- apis/ecommerce/src/main/java/com/thoughtworks/ketsu/domain/Validator.java
+++
@@ -1,8 +0,0 @@
-package com.thoughtworks.ketsu.domain;
-
-import java.util.Map;
-import java.util.Optional;
-
-public interface Validator {
-    Optional<String> validate(Map<String, Object> info);
-}
